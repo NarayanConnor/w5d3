c8d404961e344d5699830a77aa85aef56e79442b
Raboniel <imalwaysfantastic@gmail.com>
2021-07-14 12:03:57 -0400

    just saw that i typed you and not your

--- import_db.sql
+++ import_db.sql
@@ -1,1 +1,23 @@
 -- let's make a tableee
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL,
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY  (author)
+)
+
+
+CREATE TABLE question_follows (
+    
+
+
+)
